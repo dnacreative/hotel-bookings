0196c3813ae877743a7e1d8c3a6409c2fab6c36c
Eduardo Wass <ewass@ewass.mbp>
2012-08-09 18:26:31 +0200

    1.1.6
    - Themed login

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -31,63 +31,77 @@ $captcha = array(
 	'maxlength'	=> 8,
 );
 ?>
-<?php echo form_open($this->uri->uri_string()); ?>
-<table>
-	<tr>
-		<td><?php echo form_label($login_label, $login['id']); ?></td>
-		<td><?php echo form_input($login); ?></td>
-		<td style="color: red;"><?php echo form_error($login['name']); ?><?php echo isset($errors[$login['name']])?$errors[$login['name']]:''; ?></td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Password', $password['id']); ?></td>
-		<td><?php echo form_password($password); ?></td>
-		<td style="color: red;"><?php echo form_error($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
-	</tr>
+<?php //echo form_open($this->uri->uri_string()); ?>
 
-	<?php if ($show_captcha) {
-		if ($use_recaptcha) { ?>
-	<tr>
-		<td colspan="2">
-			<div id="recaptcha_image"></div>
-		</td>
-		<td>
-			<a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
-			<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
-			<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<div class="recaptcha_only_if_image">Enter the words above</div>
-			<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
-		</td>
-		<td><input type="text" id="recaptcha_response_field" name="recaptcha_response_field" /></td>
-		<td style="color: red;"><?php echo form_error('recaptcha_response_field'); ?></td>
-		<?php echo $recaptcha_html; ?>
-	</tr>
-	<?php } else { ?>
-	<tr>
-		<td colspan="3">
-			<p>Enter the code exactly as it appears:</p>
-			<?php echo $captcha_html; ?>
-		</td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirmation Code', $captcha['id']); ?></td>
-		<td><?php echo form_input($captcha); ?></td>
-		<td style="color: red;"><?php echo form_error($captcha['name']); ?></td>
-	</tr>
-	<?php }
-	} ?>
 
-	<tr>
-		<td colspan="3">
-			<?php echo form_checkbox($remember); ?>
-			<?php echo form_label('Remember me', $remember['id']); ?>
-			<?php echo anchor('/auth/forgot_password/', 'Forgot password'); ?>
-			<?php if ($this->config->item('allow_registration', 'tank_auth')) echo anchor('/auth/register/', 'Register'); ?>
-		</td>
-	</tr>
-</table>
-<?php echo form_submit('submit', 'Let me in'); ?>
-<?php echo form_close(); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+    <head>
+	
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="content-style-type" content="text/css" />
+        <meta http-equiv="content-script-type" content="text/javascript" />
+        
+        <title>Log in · Boxie Admin</title>
+        
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/black.css" media="screen, projection, tv" />  
+        <!--[if lte IE 7.0]><link rel="stylesheet" type="text/css" href="<?=base_url()?>css/ie.css" media="screen, projection, tv" /><![endif]-->
+		<!--[if IE 8.0]>
+			<style type="text/css">
+				form.fields fieldset {margin-top: -10px;}
+			</style>
+		<![endif]-->
+		
+		<script type="text/javascript" src="<?=base_url()?>js/jquery-1.3.2.min.js"></script>
+		<!-- Adding support for transparent PNGs in IE6: -->
+		<!--[if lte IE 6]>
+			<script type="text/javascript" src="<?=base_url()?>js/ddpng.js"></script>
+			<script type="text/javascript">
+				DD_belatedPNG.fix('h3 img');
+			</script>
+		<![endif]-->
+		
+    </head>
+    <body id="loginn">
+
+		<div class="box box-50 altbox">
+			<div class="boxin">
+				<div class="header">
+					<h3><img src="<?=base_url()?>css/img/black/logo.png" alt="Boxie Admin" /></h3>
+					<ul>
+						<li><a href="#" class="active">login</a></li><!-- .active for active tab -->
+						
+					</ul>
+				</div>
+				<div class="inner-form">
+
+					<form action="<?=base_url()?>index.php/auth/login" method="post" accept-charset="utf-8">
+						<div class="msg msg-info">
+							<p>Please enter your <strong>Log in</strong> details.</p>
+						</div>
+						<table cellspacing="0">
+							<tr>
+								<th><label for="some1">Username:</label></th>
+								<td><input class="txt" type="text" id="login" name="login"></td>
+							</tr>
+							<tr>
+								<th><label for="some3">Password:</label></th>
+								<td><input class="txt pwd" type="password" id="password" name="password" /></td><!-- class error for wrong filled inputs -->
+							</tr>
+							<tr>
+								<th></th>
+								<td class="tr proceed">
+									<input class="button" type="submit" value="Log in" />
+								</td>
+							</tr>
+						</table>
+
+					</form>
+					</div>
+				
+			</div>
+		</div>
+
+    </body>
+</html>
