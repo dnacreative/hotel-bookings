751dff356a1432cf8ea12a2621b1efed6318097c
Eduardo Wass <ewass@ewass.mbp>
2012-08-11 16:55:11 +0200

    1.2.2
    - Added german/french language files

---
+++ application/language/de/form_lang.php
@@ -0,0 +1,47 @@
+<?php
+
+$lang['title'] = 'Biniarroca buchungsformular';
+$lang['description'] = 'Dieses formular ist mit ssl gesichert und verfolgt eine <a href="data_de.html" target="_blank">Politik der schutz persönlicher daten</a> für ihre sicherheit. Bitte beachten sie unsere <a href="cancel_de.html" target="_blank">Stornierungsbedingungen</a> auf unserer webseite.';
+
+$lang['name'] = 'Name';
+$lang['first'] = 'Vorname';
+$lang['last'] = 'Nachname';
+$lang['passport'] = 'Passport';
+$lang['email'] = 'Email';
+$lang['phone'] = 'Telefonnummer';
+$lang['address'] = 'Addresse';
+$lang['streetad1'] = 'Addresse Linie 1';
+$lang['streetad2'] = 'Addresse Linie 2';
+$lang['city'] = 'Stadt';
+$lang['state'] = 'Bundesland / Provinz / Region';
+$lang['zipcode'] = 'Postleitzahl';
+$lang['country'] = 'Land';
+
+$lang['bookindtls'] = 'Buchungsdaten';
+$lang['arrival'] = 'Ankunft';
+$lang['departure'] = 'Abreise';
+$lang['flightno'] = 'Flugnummer';
+$lang['persons'] = 'Anzahl erwachsene';
+$lang['suites'] = 'Anzahl suites';
+$lang['doubles'] = 'Anzahl doppelzimmer';
+$lang['singles'] = 'Anzahl einzelzimmer';
+$lang['restaurant'] = 'Restaurant reservierung (sonntag geschlossen)';
+$lang['comments'] = 'Sonderwuensche (flitterwochen, unz)';
+
+
+$lang['cc'] = 'Kreditkarte';
+$lang['cc_desc'] = '(Wir werden ihnen den gesamtpreis bestätigen vor dem berechnen der 25% anzahlung, um ihre reservierung zu bestätigen bestätigen)';
+$lang['cc_type'] = 'Typ';
+$lang['cc_number'] = 'Kartennummer';
+$lang['cc_holder'] = 'Name des karteninhabers';
+$lang['cc_expiry'] = 'Verfalldatum';
+
+$lang['agreement'] = 'Ich habe alle rechtlichen vereinbarungen gelesen und akzeptiere sie';
+$lang['submit'] = 'Senden';
+
+
+$lang['bookingcode'] = 'Buchungscode';
+$lang['personalinfo'] = 'Persönliche information';
+$lang['thanks'] = 'Danke, wir haben ihre buchung erhalten. Bitte überprüfen sie alle daten, und kontaktieren sie uns wenn es einen fehler gibt.';
+$lang['thanksform'] = 'Vielen dank für ihre anfrage. Wir werden sie umgehend kontaktieren.';
+?>
---
+++ application/language/fr/form_lang.php
@@ -0,0 +1,48 @@
+<?php
+
+$lang['title'] = 'Formulaire de réserve biniarroca';
+$lang['description'] = "Ce formulaire est sécurisé avec ssl et poursuit une <a href='data_fr.html' target='_blank'>Politique d'protection des donnees</a> pour votre sécurité. Consultéz notre <a href='cancel_fr.html' target='_blank'>Politique d'annulation</a> sur notre site web.";
+
+$lang['name'] = 'Nom';
+$lang['first'] = 'Prénom';
+$lang['last'] = 'Nom';
+$lang['passport'] = 'Passeport';
+$lang['email'] = 'Email';
+$lang['phone'] = 'Téléphone';
+$lang['address'] = 'Adresse';
+$lang['streetad1'] = 'Adresse ligne 1';
+$lang['streetad2'] = 'Adresse ligne 2';
+$lang['city'] = 'Ville';
+$lang['state'] = 'État / Province / Région';
+$lang['zipcode'] = 'Code Postal';
+$lang['country'] = 'Pays';
+
+$lang['bookindtls'] = 'Détails de la réserve';
+$lang['arrival'] = 'Arrivée';
+$lang['departure'] = 'Départ';
+$lang['flightno'] = 'Numéro vol';
+$lang['persons'] = "Nombre d'adultes";
+$lang['suites'] = 'Nombre des suites';
+$lang['doubles'] = 'Nombre de doubles';
+$lang['singles'] = 'Nombre de simples';
+$lang['restaurant'] = 'Réservation restaurant (fermé le dimanche)';
+$lang['comments'] = 'Besoins particuliers (lune de miel, ..)';
+
+
+$lang['cc'] = 'Carte de crédit';
+$lang['cc_desc'] = "(Nous vous confirmerons le prix total avec vous, avant de charger votre carte l'acompte de 25% requis pour confirmer votre réservation)";
+$lang['cc_type'] = 'Type';
+$lang['cc_number'] = 'Numéro de carte de crédit';
+$lang['cc_holder'] = 'Nom du titulaire';
+$lang['cc_expiry'] = "Date d'éxpiration";
+
+$lang['agreement'] = "J'ai lu et j'accepte tous les accords juridiques.";
+$lang['submit'] = 'Soumettre';
+
+
+$lang['bookingcode'] = 'Code réserve';
+$lang['personalinfo'] = 'Info personelle';
+$lang['thanks'] = "Merci, nous avons recu votre reservation. s'il vous plaît verifiéz toutes les donnees, et contactez-nous si il ya une erreur.";
+$lang['thanksform'] = 'Merci de votre demande. On vous contactera.';
+
+?>
